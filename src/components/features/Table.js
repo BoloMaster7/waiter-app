9dc4250a2bc940aca787e602e86642f1e60cfd1c
Pawel Godula <bolomaster7@gmail.com>
2023-02-20 13:00:21 +0100

    rebuild homepage

--- src/components/features/Table.js
+++ src/components/features/Table.js
@@ -1,40 +1,39 @@
-import { useForm } from "react-hook-form";
-import { Modal, Button, Row, Col, Form } from "react-bootstrap";
-import { Link, Navigate, useParams } from "react-router-dom";
-import { useState } from "react";
+import {  Button, Row, Col, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 
-const TableForm = ({ action, actionText, ...props }) => {
-  const [people, setPeople] = useState(props.people|| '');
-  const [status, setStatus] = useState(props.author || '');
-  const [bill, setBill] = useState(props.publishedDate || '');
-  const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
-  const [category, setCategory] = useState(props.category || '');
-  const [content, setContent] = useState(props.content || '');
-  const { register, handleSubmit: validate, formState: { errors } } = useForm();
-  const [contentError, setContentError] = useState(false);
-  const [dateError, setDateError] = useState(false)
-  const { id } = useParams();
-  const maxPeople = 10
+const Table = ({  status, id, bill }) => {
 
-  const handleSubmit = () => {
-    setContentError(!content)
-    // setDateError(!publishedDate)
-    // if(content && publishedDate) {
-    //   action({ title, author, publishedDate, shortDescription, content, category });
-    // }
-  };
+ 
+  const tableStatus = status;
   return (
     
-    <Row className="mb-5">
-      <h1>Table {id} </h1>
-      <Form onSubmit={validate(handleSubmit)}>
-       
-     
-      </Form>
+    <Row>
+    <Row className="align-items-end mb-3">
+      <Col className="col-2 d-flex align-items-end justify-content-between">
+        <h2 className="mb-0">Table {id}</h2>
+      </Col>
+      <Col className="col-4">
+        <strong>Status:</strong> {status} {""}
+        {tableStatus === "Busy" && (
+          <span>
+            {""}
+            <strong>Bill: </strong>
+            {bill}
+          </span>
+        )}
+      </Col>
+      <Col className="col-6 d-flex justify-content-end">
+        <Link to={"/table/" + id}>
+          <Button variant="primary" size="sm">
+            Show more
+          </Button>
+        </Link>
+      </Col>
     </Row>
+  </Row>
  
   );
 }
 
-export default TableForm;
+export default Table;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,38 +1,30 @@
 import React from 'react';
 import Table from '../../features/Table';
-import { ListGroup, Button } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import { Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getAllTables } from '../../../redux/tablesRedux';
 
 const Home = () => {
 
-
+const tables = useSelector(getAllTables)
 
   return (
-    <ListGroup >
-    <ListGroup.Item>
-    Table 1
-    <Nav className="me-auto">
-  <Nav.Link as={NavLink} to="/tableform">Table</Nav.Link>
-</Nav>
-  </ListGroup.Item>
-  <ListGroup.Item>
-    Table 2
-    <Button variant="primary" component={NavLink} to="/tableform" >Show more
-    </Button>
-  </ListGroup.Item>
-  <ListGroup.Item>
-    Table 3
-    <Button variant="primary">Show more</Button>
-  </ListGroup.Item>
-  <ListGroup.Item>
-    Table 4
-    <Button variant="primary">Show more</Button>
-  </ListGroup.Item>
-</ListGroup>
+    <div>
+    <h1 className="mb-5">All tables</h1>
+    {tables.map((table) => (
+      <Table
+        key={table.id}
+        id={table.id}
+        number={table.id}
+        status={table.status}
+        people={table.people}
+        maxPeople={table.maxPeople}
+        bill={table.bill}
+      />
+    ))}
+  </div>
+  );
 
-  )
+
 };
 
 export default Home;
